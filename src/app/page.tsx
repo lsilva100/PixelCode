b1effd01a936766d9fc94c5f3d26ffd829ae630d
Lucas <135278098+lsilva100@users.noreply.github.com>
2024-10-07 10:57:10 -0300

    adicionando docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,23 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <header className="grid grid-cols-[150px_1fr] items-center justify-between py-4 ">
-        <Link href={""} className="flex w-[150px]">
-          <span className="font-mono text-2xl">PIXELCODE</span>
-        </Link>
+    <main className="main-background w-full">
+      <header className="grid w-full grid-cols-[150px_1fr] items-center justify-between bg-blue-500 py-4">
+        <Image
+          className="" // Adicione suas classes CSS aqui
+          src="/logo-pixel.svg" // Caminho para a imagem na pasta public
+          alt="Pixel Logo" // Texto alternativo
+          width={180} // Largura da imagem
+          height={38} // Altura da imagem
+          priority // (opcional) Carregar a imagem com alta prioridade
+        />
 
-        <nav className="flex justify-center">
-          <div className="marquee w-full ">
-            <p className="flex items-center">Este site está em construção...</p>
-          </div>
-        </nav>
+        <nav className="flex justify-center"></nav>
       </header>
 
-      <section id="hero">
+      <section className="">
         <h1 className="mx-auto max-w-lg text-center">
-          Estamos em construção...
+          Estamos em obras, mas acompanhe nas redes sociais...
         </h1>
 
         <div className="mt-10 flex flex-col items-center justify-center gap-6 sm:flex-row">
