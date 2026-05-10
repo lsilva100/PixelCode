a272d017094ddcef18bdb6ea9e4afa54181c6e24
Lucas <135278098+lsilva100@users.noreply.github.com>
2026-05-10 01:53:43 -0300

    Criação do Header com Toggle funcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import CountdownTimer from "./CountdownTimer";
+import Toggle from "./components/Toggle";
 
 export default function Home() {
   return (
@@ -14,17 +14,36 @@ export default function Home() {
         className="absolute inset-0"
       />
 
-      <header className="relative z-10 grid w-full grid-cols-[135px_1fr] items-center justify-between bg-blue-500 py-4">
-        <Image
-          className="" // Adicione suas classes CSS aqui
-          src="/logo-pixel.svg" // Caminho para a imagem na pasta public
-          alt="Pixel Logo" // Texto alternativo
-          width={180} // Largura da imagem
-          height={40} // Altura da imagem
-          priority // (opcional) Carregar a imagem com alta prioridade
-        />
+      <header className="relative z-10 grid w-full grid-cols-3 gap-4 items-center bg-black py-4 px-4 text-white">
+        <div className="flex items-center justify-start">
+          <Image
+            src="/logo_l2.svg"
+            alt="Pixel Logo"
+            width={90}
+            height={40}
+            priority
+          />
+        </div>
 
-        <nav className="flex justify-center"></nav>
+        <nav className="flex items-center justify-center gap-6 text-sm uppercase tracking-[0.2em]">
+          <Link href="#">About us</Link>
+          <Link href="#">Projects</Link>
+          <Link href="#">Contact</Link>
+        </nav>
+
+        <div className="flex items-center justify-end">
+      <div className="inline-flex items-center gap-5 rounded-full border border-zinc-700 px-4 py-2">
+        <span className="text-sm uppercase tracking-wider text-zinc-400">
+        LSilva
+        </span>
+
+      <Toggle />
+
+        <span className="text-sm uppercase tracking-wider text-zinc-400">
+        LMateus
+          </span>
+          </div>
+          </div>
       </header>
 
       <section className="relative z-10">
