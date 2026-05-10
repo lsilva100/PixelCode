3c971350d4cd7769b92584a3627d95368d5ef224
Lucas <lucasnba750@hotmail.com>
2026-05-10 09:55:55 -0300

    fix(tailwind): fixed config content paths for /components and /projects

--- components/ProjectGallery.tsx
+++ components/ProjectGallery.tsx
@@ -6,7 +6,7 @@ import { ProjectBinarySearch } from "../projects/Projects";
 
 export default function ProjectsGallery() {
   return (
-      <div className="bg-black mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
         <ProjectBinarySearch />
         <ProjectLinearSearch />
         <ProjectHashTable />
