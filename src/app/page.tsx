5b774c60a62b34eeeedc91c80b214b15fa480a20
Lucas <135278098+lsilva100@users.noreply.github.com>
2026-05-13 18:43:56 -0300

    Adição do footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Toggle from "../../components/Toggle";
 export default function Home() {
   return (
     <main className="w-full min-h-screen bg-black">
-     <header className="relative z-10 flex w-full items-center justify-between bg-gradient-to-tl from-black to-gray-900 py-6 px-8">
+    <header className="relative z-10 flex w-full items-center justify-between bg-gradient-to-tl from-black to-gray-900 py-6 px-8">
         <div className="flex-shrink-0">
           <Image
             className="" 
@@ -30,11 +30,11 @@ export default function Home() {
 
     <Toggle />
 
-    <span>LMATEUS</span>
+    <span>LNEVES</span>
   </div>
-      </header>
+    </header>
 
-       <div className="bg-gradient-to-tl from-black to-gray-900 text-white w-full p-6">
+    <div className="bg-gradient-to-tl from-black to-gray-900 text-white w-full p-16">
         <div className="flex flex-col gap-4 mb-8">
           <p className="text-sm uppercase tracking-widest opacity-70">Autores: Lucas da Silva & Lucas das Neves</p>
           <h1 className="text-4xl font-bold text-white">Projetos</h1>
@@ -43,8 +43,13 @@ export default function Home() {
         <div>
           <ProjectsGallery />
         </div>
-      </div>
+    </div>
+
+    <footer className="bg-gradient-to-tl from-black to-gray-900 text-white w-full p-6">
+      <p className="text-center opacity-70">© 2026 LSILVA & LNEVES. Todos os direitos reservados.</p>
+    </footer>
+
     </main>
-  );
+  );  
 }
 
