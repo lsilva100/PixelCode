4a02e693b431dbb79b744fc1e2727c26fcb46ed6
Lucas <lucasnba750@hotmail.com>
2026-05-14 09:43:51 -0300

    feat: swap animations on projects

--- components/ProjectGallery.tsx
+++ components/ProjectGallery.tsx
@@ -1,14 +1,24 @@
 import ProjectCard, { ProjectProps } from "./ProjectCard";
 import {projectDataL1} from "../projects/descriptions";
 import { Project } from "next/dist/build/swc/types";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function ProjectsGallery(props: { projectsFromAuthor: ProjectProps[] }) {
   return (
-      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
+    <AnimatePresence mode="popLayout">
+      <motion.div
+        layout
+        className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6"
+      >
         {props.projectsFromAuthor.map((project, index) => (
-          <ProjectCard key={index} props={project} />
+          <ProjectCard
+            key={project.Info.title}
+            props={project}
+            direction={index % 2 === 0 ? 1 : -1}
+          />
         ))}
-      </div>      
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
